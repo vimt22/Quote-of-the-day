bb790770de3dad8c71edbc1d49a3e835869d5525
vimt22 <77280219+vimt22@users.noreply.github.com>
2024-03-14 22:47:26 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
+  title: 'Quote of the day',
+  description: 'write me a quote so I can read in the morning to motivate myself to live my life better.',
   copyright: '',
   privacy_policy: '',
   default_language: 'en-US',
