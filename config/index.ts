dd4f485438addb20488d58abca06da6be5da77cb
vimt22 <77280219+vimt22@users.noreply.github.com>
2024-03-14 22:51:18 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -5,7 +5,7 @@ export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Quote of the day',
   description: 'write me a quote so I can read in the morning to motivate myself to live my life better.',
-  copyright: 'A22 AI',
+  copyright: 'A22 Studio',
   privacy_policy: 'https://myappterms.com/EF5Y2Z/1/en',
   default_language: 'en-US',
 }
